019f5c81a5d6bd19b8631ec0622af88b672d9cff
Sarang Kolhe <sarang.kolhe79@gmail.com>
2026-07-06 20:36:10 +0530

    lb changes

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -1,3 +1,4 @@
+
 # Create the Paymentology application load balancer
 resource "aws_lb" "paymentology_alb" {
   name               = "${var.project_name}-alb"
@@ -32,9 +33,48 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
+default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "cloudfront_origin_http" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 1
+
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "Via"
+      values           = ["*.cloudfront.net (CloudFront)"]
+    }
+    }
+  }
+
+resource "aws_lb_listener_rule" "api_gateway_origin_http" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 2
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "User-Agent"
+      values           = ["AmazonAPIGateway_*"]
+    }
   }
 }
 
