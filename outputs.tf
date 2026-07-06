c05a822a65306cdc3f4fc358f92663bf30f1acb5
Sarang Kolhe <sarang.kolhe79@gmail.com>
2026-07-06 15:26:27 +0530

    output

--- outputs.tf
+++ outputs.tf
@@ -27,8 +27,3 @@ output "vpc_id" {
   description = "VPC ID"
   value       = module.networking.vpc_id
 }
-
-# output "monitoring_log_group" {
-#   description = "CloudWatch log group name"
-#   value       = module.monitoring.log_group_name
-# }
