8ef98cebcc56e2a7f1dbf03839c877643e7cd2d7
Sarang Kolhe <sarang.kolhe79@gmail.com>
2026-07-09 23:28:15 +0530

    updated

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "alb_dns_name" {
 
 output "api_gateway_url" {
   description = "Invokable URL for the API Gateway"
-  value       = join("/", [module.api_gateway.invoke_url, "index.html"])
+  value       = "${module.api_gateway.invoke_url}/index.html"
 }
 
 output "cloudfront_domain_name" {
